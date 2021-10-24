8934fcc3e469f41524439e3f3d6b275ea6d480e6
Krivobarac <gasparovicsasa@yahoo.com>
2021-10-24 20:52:31 +0200

    Responsivnes Menu checkbox checked = body overflow hidden

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
-document.getElementById("container").addEventListener("click", function() {
-    document.getElementById("menu-checkbox").checked = false;
+const menuCheckBox = document.getElementById("menu-checkbox");
+const container = document.getElementById("container");
+
+menuCheckBox.addEventListener("change", function() {
+    if(menuCheckBox.checked) {
+        document.body.style.overflow = 'hidden'
+        container.classList.add('container-disabled');
+    } else {
+        document.body.style.overflow = 'auto'
+        container.classList.remove('container-disabled')
+    }
+})
+
+container.addEventListener("click", function() {
+    console.log(menuCheckBox.checked)
+    if(menuCheckBox.checked) {
+        menuCheckBox.click();
+        menuCheckBox.checked = false;
+    }
 })
